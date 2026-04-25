f04ff85016c242b08fb72475ea383cb233bf9803
prakashsingh0 <singhprakash545@gmail.com>
2026-04-25 20:32:12 +0530

    create address class

---
+++ project/address.py
@@ -0,0 +1,9 @@
+class Address:
+    """A class representing a physical address"""
+
+    def __init__(self,street, city, state, zip_code):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.state = zip_code
+        
--- project/customer.py
+++ project/customer.py
@@ -1,3 +1,5 @@
+from address import Address
+
 class Customer:
     def __init__(self,first_name:str,middle_name:str,last_name:str,email:str,phone:str,account_type:str,account_number:str,customer_id:str,_balance = 0):
         self.customer_id = customer_id
@@ -23,7 +25,7 @@ class Customer:
 \nAccount number : {self.account_number}
 \nName : {self.first_name} {self.middle_name} {self.last_name}
 \nAccount Status : {'Active' if self._is_active else "Deactive"}"""
-        """update customer Profile details"""
+    """update customer Profile details"""
     def update_details(self,address={},dob=None,aadhar_card_no=None,pan_card_no = None):
         """Validate data"""
         if dob is not None:     
@@ -31,7 +33,7 @@ class Customer:
         if address is not None:
             self.address = address
         if aadhar_card_no is not None:
-            self.aadhar_card_no = aadhar_card_no
+            self.aadhar_card_no = address
         if pan_card_no is not None:
             self.pan_card_no  = pan_card_no
         if self.aadhar_card_no:
@@ -43,6 +45,7 @@ class Customer:
         print('*'*50)
         print(f'Customer ID: {self.customer_id}')
         print(f'Account number : {self.account_number}')
+        print(f'Account Type : {self.account_type}')
         print(f'Name : {self.first_name} {self.middle_name} {self.last_name}')
         print(f'Account balance : {self._balance}')
         print(f'Phone Number : {self.phone}')
@@ -60,12 +63,13 @@ class Customer:
                 raise ValueError("Amount must be Greater then 0")
             if amount >=0:
                 self._balance += amount
+                self.transactions += 1
             return True
           else:
               print('Your Account is not Activated')
         except Exception as e:
             print(f'Something went wrong {e:.2f}')
-        
+    """Withdraw cash from account"""
     def withdraw(self,amount):
         try:
             if amount > self._balance:
@@ -74,6 +78,7 @@ class Customer:
                 raise ValueError("Amount must be greater then 0")
             if amount <= self._balance and amount >= 0:
                 self._balance -= amount
+                self.transactions += 1
             return True
         except Exception as e:
            print(f'Something went wrong {e:.2f}')
